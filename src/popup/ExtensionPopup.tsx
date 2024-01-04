95078dec1d6d0860dc406f8ae832da8479037c3a
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-01-04 17:28:52 +0900

    feat: popupのレイアウトをざっくり実装

--- src/popup/ExtensionPopup.tsx
+++ src/popup/ExtensionPopup.tsx
@@ -5,10 +5,19 @@ import {
 	Button,
 	Container,
 	Flex,
+	NumberInput,
+	Select,
 	Stack,
+	Table,
+	TextInput,
 	Title,
 } from "@mantine/core";
-import { IconSettings } from "@tabler/icons-react";
+import {
+	IconSettings,
+	IconSquareMinus,
+	IconSquareMinusFilled,
+	IconTextScan2,
+} from "@tabler/icons-react";
 import { type FC, useCallback } from "react";
 import {
 	OptionsPageUrl,
@@ -27,8 +36,8 @@ export const ExtensionPopup: FC = () => {
 	return (
 		<AppShell
 			h={"min-height"}
-			w={300}
-			padding="xs"
+			w={560}
+			// padding="xs"
 			header={{
 				height: 32,
 			}}
@@ -47,7 +56,84 @@ export const ExtensionPopup: FC = () => {
 					</ActionIcon>
 				</Flex>
 			</AppShell.Header>
-			<AppShell.Main>あああ</AppShell.Main>
+			<AppShell.Main>
+				<Main />
+			</AppShell.Main>
 		</AppShell>
+	);
+};
+
+const Main: FC = () => {
+	return (
+		<Stack>
+			<Table horizontalSpacing={4}>
+				<Table.Thead>
+					<Table.Tr>
+						<Table.Th>品目</Table.Th>
+						<Table.Th>カテゴリ</Table.Th>
+						<Table.Th>金額</Table.Th>
+						<Table.Th>Act</Table.Th>
+					</Table.Tr>
+				</Table.Thead>
+				<Table.Tbody>
+					<Table.Tr>
+						<Table.Td>
+							<TextInput size="xs" />
+						</Table.Td>
+						<Table.Td>
+							<CategorySelect />
+						</Table.Td>
+						<Table.Td>
+							<NumberInput size="xs" leftSection={"¥"} hideControls />
+						</Table.Td>
+						<Table.Td>
+							<ActionIcon variant={"transparent"} color="green">
+								<IconSquareMinusFilled size={20} />
+							</ActionIcon>
+						</Table.Td>
+					</Table.Tr>
+				</Table.Tbody>
+			</Table>
+			<Stack px={4}>
+				<Select placeholder="出金元" />
+				{/* DateInput */}
+				<Select placeholder="お店" />
+
+				<Button variant="light" color="green">
+					<IconTextScan2 size={20} />
+					ページから選択して入力
+				</Button>
+				<Button variant="filled" color="green" fullWidth>
+					登録
+				</Button>
+			</Stack>
+
+			<Box h={300} />
+		</Stack>
+	);
+};
+
+const CategorySelect: FC<{}> = () => {
+	return (
+		<Box>
+			<Select
+				placeholder="カテゴリ"
+				searchable
+				selectFirstOptionOnChange
+				size="xs"
+				comboboxProps={{
+					position: "bottom-start",
+					offset: 0,
+					width: "300px",
+				}}
+				data={[
+					{
+						group: "Frontend",
+						items: ["ReactReactReactReactReactReactReact", "Angular"],
+					},
+					{ group: "Backend", items: ["Express", "Django"] },
+				]}
+			/>
+		</Box>
 	);
 };
