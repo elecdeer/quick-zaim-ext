eb1ebbb0c71172beb9d1b010c49f4b3617a7659b
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-01-27 23:36:44 +0900

    feat: API叩いて登録できるようにした

--- src/popup/components/PopupMain.tsx
+++ src/popup/components/PopupMain.tsx
@@ -10,6 +10,7 @@ import {
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import { useListState } from "@mantine/hooks";
+import { showNotification } from "@mantine/notifications";
 import { useStorage } from "@plasmohq/storage/hook";
 import {
 	IconBarcode,
@@ -17,9 +18,11 @@ import {
 	IconSquareMinusFilled,
 	IconTextScan2,
 } from "@tabler/icons-react";
-import { type FC, useCallback, useState } from "react";
+import { type FC, useCallback, useMemo, useState } from "react";
+import { formatToYYYYMMDD } from "~lib/dateUtil";
 import type { AccessTokenPair } from "~lib/oauth";
 import { oauthAccessTokenStore } from "~lib/store";
+import { type ZaimPaymentReq, postZaimPayment } from "~lib/zaim/postPayment";
 import { AccountSelect, type ZaimAccount } from "./AccountSelect";
 import { CategorySelect } from "./CategorySelect";
 import { PaymentPlaceSelect, type ZaimPlace } from "./PaymentPlaceSelect";
@@ -44,19 +47,31 @@ const createDefaultRecord = () => ({
 	amount: 1,
 });
 
+type PaymentRecordItem = {
+	uid: string;
+	itemName: string;
+	categoryId: string | undefined;
+	genreId: string | undefined;
+	price: number | undefined;
+	amount: number;
+};
+
+type ValidRecordItem = {
+	uid: string;
+	itemName: string;
+	categoryId: string;
+	genreId: string;
+	price: number;
+	amount: number;
+};
+
 const PopupMainAuthorized: FC = () => {
 	// TODO: 入力項目はstorageに保存するようにする？
 	// TODO: "ACT"の部分をハンバーガーアイコンにして、メニューをいくつか用意する
 	// 	今歯車アイコンになっている所をメニューにしても良いな
 
-	const [paymentRecords, paymentRecordsController] = useListState<{
-		uid: string;
-		itemName: string;
-		categoryId: string | undefined;
-		genreId: string | undefined;
-		price: number | undefined;
-		amount: number;
-	}>([createDefaultRecord()]);
+	const [paymentRecords, paymentRecordsController] =
+		useListState<PaymentRecordItem>([createDefaultRecord()]);
 
 	const [selectedPaymentPlaceUid, setSelectedPaymentPlaceUid] = useState<
 		string | undefined
@@ -64,6 +79,9 @@ const PopupMainAuthorized: FC = () => {
 	const [selectedAccountId, setSelectedAccountId] = useState<
 		string | undefined
 	>(undefined);
+	const [selectedDate, setSelectedDate] = useState<Date | undefined>(
+		new Date(),
+	);
 
 	const handleSelectPaymentPlace = useCallback(
 		({ placeUid, accountId }: ZaimPlace) => {
@@ -83,6 +101,74 @@ const PopupMainAuthorized: FC = () => {
 	const appendRecord = useCallback(() => {
 		paymentRecordsController.append(createDefaultRecord());
 	}, [paymentRecordsController]);
+
+	const validRecords = useMemo(
+		() =>
+			paymentRecords.filter((record): record is ValidRecordItem => {
+				if (record.itemName === "") return false;
+				if (record.categoryId === undefined) return false;
+				if (record.genreId === undefined) return false;
+				if (record.price === undefined) return false;
+				return true;
+			}),
+		[paymentRecords],
+	);
+	const isRegisterButtonActive =
+		validRecords.length > 0 && selectedAccountId !== undefined;
+
+	const handleClickRegister = useCallback(() => {
+		if (selectedAccountId === undefined) return;
+
+		console.log(validRecords);
+
+		const date = new Date();
+		const receiptId = Math.floor(date.getTime() / 1000); // ユニークなID
+
+		const paymentReqPayloads = validRecords.map(
+			(record) =>
+				({
+					mapping: 1,
+					category_id: Number(record.categoryId),
+					genre_id: Number(record.genreId),
+					amount: (record.price ?? 0) * record.amount,
+					date: formatToYYYYMMDD(date),
+					from_account_id: Number(selectedAccountId),
+					name: record.itemName,
+					place_uid: selectedPaymentPlaceUid,
+					receipt_id: receiptId,
+					comment: "test",
+				}) satisfies ZaimPaymentReq,
+		);
+
+		console.log({ paymentReqPayloads });
+
+		Promise.all(paymentReqPayloads.map((payload) => postZaimPayment(payload)))
+			.then((res) => {
+				console.log("all posted");
+				console.log({ res });
+
+				paymentRecordsController.setState([createDefaultRecord()]);
+				showNotification({
+					title: "登録完了",
+					message: `${res.length}件の支出を登録しました`,
+					color: "teal",
+				});
+			})
+			.catch((err) => {
+				console.error(err);
+				showNotification({
+					title: "登録失敗",
+					message: "支出の登録に失敗しました",
+					color: "red",
+				});
+			});
+	}, [
+		validRecords,
+		selectedAccountId,
+		selectedPaymentPlaceUid,
+		paymentRecordsController,
+		paymentRecords.length,
+	]);
 
 	return (
 		<Stack>
@@ -203,7 +289,11 @@ const PopupMainAuthorized: FC = () => {
 						monthLabelFormat="YYYY年M月"
 						monthsListFormat="M月"
 						weekdayFormat={(date) => "日月火水木金土"[date.getDay()]}
+						required
+						value={selectedDate}
 						onChange={(date) => {
+							if (date === null) return;
+							setSelectedDate(date);
 							console.log(date);
 						}}
 					/>
@@ -226,7 +316,12 @@ const PopupMainAuthorized: FC = () => {
 					</Button>
 				</SimpleGrid>
 
-				<Button variant="filled" fullWidth>
+				<Button
+					variant="filled"
+					fullWidth
+					onClick={handleClickRegister}
+					disabled={!isRegisterButtonActive}
+				>
 					登録
 				</Button>
 			</Stack>
