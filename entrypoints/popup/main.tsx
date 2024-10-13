57521d44bcfdf1724b9960eb5b3ec238811e4ae3
elecdeer <37701077+elecdeer@users.noreply.github.com>
2024-10-13 15:30:10 +0900

    chore: fix lintとか

--- entrypoints/popup/main.tsx
+++ entrypoints/popup/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./style.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+// biome-ignore lint/style/noNonNullAssertion: index.htmlに必ず存在する
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 );
