9c98aaebcc6ffbd5e8b8517d9d71515fa1f23e5c
elecdeer <37701077+elecdeer@users.noreply.github.com>
2025-01-26 14:49:04 +0900

    feat: openaiをテスト

--- packages/extention/entrypoints/popup/App.tsx
+++ packages/extention/entrypoints/popup/App.tsx
@@ -2,6 +2,7 @@ import reactLogo from "@/assets/react.svg";
 import { useCallback, useState } from "react";
 import wxtLogo from "/wxt.svg";
 import "./App.css";
+import OpenAI from "openai";
 import { browser } from "wxt/browser";
 
 function App() {
@@ -17,11 +18,16 @@ function App() {
 		console.log("activeTab", tab);
 		if (!tab || !tab.id) return;
 
-		const res = await browser.scripting.executeScript({
+		const [res] = await browser.scripting.executeScript({
 			target: { tabId: tab.id },
 			files: ["content-scripts/extract.js"],
 		});
 		console.log(res);
+
+		if (res.result) {
+			const extractResult = await extractByOpenAI(res.result);
+			console.log(extractResult);
+		}
 	}, []);
 
 	return (
@@ -49,5 +55,116 @@ function App() {
 		</>
 	);
 }
+
+const extractByOpenAI = async (pageHtml: string) => {
+	const apiKey = localStorage.getItem("openai-api-key");
+	if (!apiKey) {
+		console.warn("OpenAI API key is not set");
+		return;
+	}
+
+	const openai = new OpenAI({
+		apiKey,
+		// TODO:; 後でサーバ側に移動してこれを外す
+		dangerouslyAllowBrowser: true,
+	});
+
+	const response = await openai.chat.completions.create({
+		model: "gpt-4o-mini",
+		messages: [
+			{
+				role: "system",
+				content: [
+					{
+						text: "入力の請求書の情報を読み取り出力してください。\n割引についてもitemの1つとして扱ってください。\nsumPriceが各itemのpriceYen x amountの合計になるように確認してください。",
+						type: "text",
+					},
+				],
+			},
+			{
+				role: "user",
+				content: [
+					{
+						text: pageHtml,
+						type: "text",
+					},
+				],
+			},
+		],
+		response_format: {
+			type: "json_schema",
+			json_schema: {
+				name: "receipt",
+				schema: {
+					type: "object",
+					required: ["shopName", "date", "sumPrice", "receiptId", "items"],
+					properties: {
+						date: {
+							type: "string",
+							description: "YYYY-MM-DD形式の購入日",
+						},
+						items: {
+							type: "array",
+							items: {
+								type: "object",
+								required: ["name", "category", "priceYen", "amount"],
+								properties: {
+									name: {
+										type: "string",
+										description: "商品名",
+									},
+									amount: {
+										type: "integer",
+										description: "商品の個数",
+									},
+									category: {
+										type: "string",
+										description: "商品のカテゴリ",
+									},
+									priceYen: {
+										type: "integer",
+										description: "商品の金額",
+									},
+								},
+								additionalProperties: false,
+							},
+							description: "A list of items purchased in the transaction.",
+						},
+						shopName: {
+							type: "string",
+							description: "購入した店舗やサイトの名前",
+						},
+						sumPrice: {
+							type: "integer",
+							description: "The total price of all items on the receipt.",
+						},
+						receiptId: {
+							type: "string",
+							description: "A unique identifier for the receipt.",
+						},
+					},
+					additionalProperties: false,
+				},
+				strict: true,
+			},
+		},
+		temperature: 1,
+		max_completion_tokens: 2048,
+		top_p: 1,
+		frequency_penalty: 0,
+		presence_penalty: 0,
+	});
+
+	console.log(response);
+
+	const resultRaw = response.choices[0].message.content;
+
+	console.log(resultRaw);
+	if (!resultRaw) {
+		return null;
+	}
+
+	return JSON.parse(resultRaw);
+};
 
 export default App;
