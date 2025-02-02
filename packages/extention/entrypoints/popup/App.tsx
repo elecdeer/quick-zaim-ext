6f3597210ab6946a9dad077280ded2db34a868a8
elecdeer <37701077+elecdeer@users.noreply.github.com>
2025-02-02 15:46:46 +0900

    feat: @hono/oidc-authで認証を実装

--- packages/extention/entrypoints/popup/App.tsx
+++ packages/extention/entrypoints/popup/App.tsx
@@ -1,13 +1,10 @@
-import reactLogo from "@/assets/react.svg";
-import { useCallback, useState } from "react";
-import wxtLogo from "/wxt.svg";
-import "./App.css";
 import OpenAI from "openai";
+import { useCallback } from "react";
 import { browser } from "wxt/browser";
 
-function App() {
-	const [count, setCount] = useState(0);
+import "./App.css";
 
+function App() {
 	const handleClick = useCallback(async () => {
 		const tabs = await browser.tabs.query({
 			active: true,
@@ -31,28 +28,74 @@ function App() {
 	}, []);
 
 	return (
-		<>
-			<div>
-				<a href="https://wxt.dev" target="_blank" rel="noreferrer">
-					<img src={wxtLogo} className="logo" alt="WXT logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>WXT + React</h1>
-			<div className="card">
-				<button type="button" onClick={handleClick}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the WXT and React logos to learn more
-			</p>
-		</>
+		<div className="card">
+			<button type="button" onClick={handleClick}>
+				Extract
+			</button>
+			<button
+				type="button"
+				onClick={async () => {
+					const url = new URL("http://localhost:8787/login");
+					url.searchParams.set(
+						"return-to",
+						"https://efpgpbmleoemnhndmngfoinonbmbibed.chromiumapp.org",
+					);
+
+					const res = await browser.identity.launchWebAuthFlow({
+						interactive: true,
+						url: url.toString(),
+					});
+					console.log(res);
+				}}
+			>
+				Login
+			</button>
+			<button
+				type="button"
+				onClick={async () => {
+					const url = new URL("http://localhost:8787/hello");
+
+					try {
+						const res = await fetch(url, {
+							method: "GET",
+						});
+
+						if (res.ok) {
+							const text = await res.text();
+							console.log("res", text);
+						} else {
+							console.warn("res not ok", res);
+
+							console.warn("redirected", res.redirected);
+							console.warn("status", res.status);
+							console.warn("statusText", res.statusText);
+							console.warn("url", res.url);
+						}
+					} catch (e) {
+						console.warn("catch", e);
+
+						if (e instanceof TypeError) {
+							console.warn("TypeError", e.message);
+						}
+					}
+				}}
+			>
+				Hello
+			</button>
+
+			<button
+				type="button"
+				onClick={async () => {
+					const url = new URL("http://localhost:8787/logout");
+					const res = await fetch(url, {
+						method: "GET",
+					});
+					console.log(res);
+				}}
+			>
+				Logout
+			</button>
+		</div>
 	);
 }
 
--- packages/workers/src/index.ts
+++ packages/workers/src/index.ts
@@ -1,13 +1,68 @@
+import {
+	getAuth,
+	oidcAuthMiddleware,
+	processOAuthCallback,
+	revokeSession,
+} from "@hono/oidc-auth";
 import { Hono } from "hono";
 
+import { createMiddleware } from "hono/factory";
+import { HTTPException } from "hono/http-exception";
 import { extractionHtmlRoute } from "./handlers/extraction/html";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-	return c.text("Hello Hono!");
+app.get("/callback", (c) => {
+	return processOAuthCallback(c);
 });
 
-app.route("/extraction/html", extractionHtmlRoute);
+// https://github.com/honojs/middleware/issues/760#issuecomment-2520606683
+app.use(
+	createMiddleware(async (c, next) => {
+		console.log("c.req.path", c.req.path);
+
+		// /login以外のパスでは認証されていない場合に401を返す
+		if (c.req.path !== "/login") {
+			const auth = await getAuth(c);
+			if (auth === null) {
+				console.log("Unauthorized");
+				throw new HTTPException(401, {
+					message: "Unauthorized",
+				});
+			}
+		}
+
+		await next();
+	}),
+);
+
+app.get("/login", async (c, next) => {
+	console.log("/login endpoint");
+	const urlAfterLogin = c.req.query("return-to");
+	console.log("urlAfterLogin", urlAfterLogin);
+
+	const auth = await getAuth(c);
+	console.log("auth", auth);
+	if (urlAfterLogin !== undefined && auth !== null) {
+		// oidcAuthMiddlewareでの認証後に遷移する先
+		return c.redirect(urlAfterLogin);
+	}
+
+	await next();
+});
+
+app.get("/logout", async (c) => {
+	await revokeSession(c);
+	return c.text("You have been successfully logged out!");
+});
+
+app.use(oidcAuthMiddleware());
+
+app.get("/hello", async (c) => {
+	const auth = await getAuth(c);
+	return c.text(`Hello <${auth?.email}>!`);
+});
+
+// app.route("/extraction/html", extractionHtmlRoute);
 
 export default app;
