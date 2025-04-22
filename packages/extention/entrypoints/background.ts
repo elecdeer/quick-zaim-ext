a913ef919db0ce8d17d56ea040bd720826bfa0c9
elecdeer <37701077+elecdeer@users.noreply.github.com>
2025-04-23 01:43:48 +0900

    chore: update wxt

--- packages/extention/entrypoints/background.ts
+++ packages/extention/entrypoints/background.ts
@@ -1,5 +1,5 @@
 import { browser } from "wxt/browser";
-import { defineBackground } from "wxt/sandbox";
+import { defineBackground } from "wxt/utils/define-background";
 
 export default defineBackground(() => {
 	console.log("Hello background!", { id: browser.runtime.id });
